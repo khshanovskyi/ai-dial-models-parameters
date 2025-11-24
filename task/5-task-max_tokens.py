ef2151fc4d23a050d959bbe2fa909aa43b0222c7
vadim_dissa <vadim_dissa@epam.com>
2025-11-24 18:55:02 -0500

    Complete task 5

--- task/5-task-max_tokens.py
+++ task/5-task-max_tokens.py
@@ -5,9 +5,13 @@ from task.app.main import run
 #  User massage: What is token when we are working with LLM?
 
 run(
-    deployment_name='gpt-4o',
-    # TODO:
-    #  Use `max_tokens` parameter with value 10
+    deployment_name="gpt-4o",
+    print_request=True, # Switch to False if you do not want to see the request in console
+    print_only_content=False, # Switch to True if you want to see only content from response
+    n=5,  # Number of chat completion choices to generate for each input message,
+    temperature=1.0, # Adjust the temperature parameter here
+    seed=123, # Use seed parameter to make output more deterministic
+    max_tokens=10, # Set the maximum length of the AI's response
 )
 
 # Previously, we have seen that the `finish_reason` in choice was `stop`, but now it is `length`, and if you check the
